b8b99753bb4eeb17a451de224cdca83094a9b9a4
Guillaume Archambault <guillaume.archambault@bnc.ca>
2020-06-03 19:44:18 -0400

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 import Box from "@material-ui/core/Box";
 
 const Home = () => {
-  return <Box pt={0}>Welcome home, testing my pipeline with aws.</Box>;
+  return <Box pt={0}>Welcome home, testing my pipeline with aws. With Qooch and Kid</Box>;
 };
 
 export default Home;
